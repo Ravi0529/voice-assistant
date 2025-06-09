a3a8367e6a4a39fc50ffed4593993c94a3e5b474
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-06-09 11:49:24 +0530

    tools-and-main.py-update

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+# Take user input (Speech)
+
+import speech_recognition as sr
+
+
 def main():
-    print("Voice assistant!")
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        # r.pause_threshold = 3
+
+        while True:
+            print("Listening...")
+            audio = r.listen(source)
+
+            print("Processing Audio...")
+            try:
+                stt = r.recongize_google(audio)
+                print("User: ", stt)
+            except sr.UnknownValueError:
+                print("Apologies, the audio wasn't clear enough.")
+            except sr.RequestError as e:
+                print("There was an issue retrieving results. Error: {0}".format(e))
 
 
 if __name__ == "__main__":
